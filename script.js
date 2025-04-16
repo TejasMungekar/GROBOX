93a02529b21287babde9c409d095aa5fc52f2697
TejasMungekar <tejas.mungekar4002@gmail.com>
2025-04-16 11:23:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function rotateSlides(bannerId) {
+    const slides = document.querySelectorAll(`#${bannerId} .banner-slide`);
+    let current = 0;
+    setInterval(() => {
+      slides[current].classList.remove('active');
+      current = (current + 1) % slides.length;
+      slides[current].classList.add('active');
+    }, 4000);
+  }
+
+  rotateSlides('banner1');
+  rotateSlides('banner2');
